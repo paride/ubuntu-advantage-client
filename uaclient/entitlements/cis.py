2f3a3046ac62d50a1d5d826a8ac8df9da894d293
Chad Smith <chad.smith@canonical.com>
2020-12-10 12:54:50 -0700

    cis: add PPA keyring for CIS and rename service from cis-audit

--- uaclient/entitlements/cis.py
+++ uaclient/entitlements/cis.py
@@ -4,7 +4,7 @@ from uaclient.entitlements import repo
 class CISEntitlement(repo.RepoEntitlement):
 
     help_doc_url = "https://ubuntu.com/security/certifications#cis"
-    name = "cis-audit"
+    name = "cis"
     title = "CIS Audit"
     description = "Center for Internet Security Audit Tools"
     repo_key_file = "ubuntu-securitybenchmarks-keyring.gpg"
